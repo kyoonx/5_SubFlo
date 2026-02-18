76aaa4dbf1b7c69c1d4c7675ecff2df05bcae88e
Tom <pitupoomatom139@gmail.com>
2026-02-17 23:41:29 -0600

    Add `api_cost_per_month`

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from dashboard.views import (SubscriptionList, api_all_active_subscriptions, email_message_list, subscription_detail, email_message_detail, subscription_chart, subscriptions_per_month_chart)
+from dashboard.views import (SubscriptionList, api_all_active_subscriptions, api_cost_per_month, email_message_list, subscription_detail, email_message_detail, subscription_chart, subscriptions_per_month_chart)
 
 urlpatterns = [
     path("", SubscriptionList.as_view(), name="subscription-list-url"),  
@@ -10,6 +10,7 @@ urlpatterns = [
     path("email_message/<uuid:pk>", email_message_detail, name="email_message_detail-url"),
     
     path("api/subscriptions/active/", api_all_active_subscriptions, name="api-active-subscriptions-url"),
+    path("api/subscriptions/cost_per_month/", api_cost_per_month, name="api-cost-per-month-url"),
     
     path("subscriptions/chart.png", subscription_chart, name="subscription-chart-url"),
     path("subscriptions/monthly-chart.png", subscriptions_per_month_chart, name="subscriptions-monthly-chart-url"),
