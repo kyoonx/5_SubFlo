e183b8e3ee84e701f445c1765f1c014c8a3fb37d
kyoonx <lilyliucute@gmail.com>
2026-02-26 21:34:23 -0600

    modifications to signup.html

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,9 @@ from django.views import View
 from accounts.models import UserProfile
 # from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 ############################################################
 #################### Internal API Views ####################
@@ -45,3 +47,24 @@ def api_verify_user_id(request):
         return HttpResponse(f"This user_id ({profile_uuid}) is existing/valid.", status=200)
     except UserProfile.DoesNotExist:
         return HttpResponse(f"This user_id ({profile_uuid}) does not exist.", status=404)
+
+class SignupView(View):
+    def get(self, request):
+        # If user is already logged in, send them to the dashboard
+        if request.user.is_authenticated:
+            return redirect('subscription-list-url')
+
+        form = UserCreationForm()
+        return render(request, 'accounts/signup.html', {'form': form})
+
+    def post(self, request):
+        if request.user.is_authenticated:
+            return redirect('subscription-list-url')
+
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log the user in right after signup
+            return redirect('subscription-list-url')  # same as LOGIN_REDIRECT_URL
+
+        return render(request, 'accounts/signup.html', {'form': form})
