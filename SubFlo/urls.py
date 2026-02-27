a8aab83f24d235a4080ea3e8a38c64ea8484eeda
kyoonx <lilyliucute@gmail.com>
2026-02-26 22:28:10 -0600

    Added signup

--- SubFlo/urls.py
+++ SubFlo/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from SubFlo.views import redirect_root_view
-
+from accounts.views import SignupView
+3s
 urlpatterns = [
     path("", redirect_root_view),
     path("admin/", admin.site.urls),
     path("dashboard/", include("dashboard.urls")),
-    path("api/", include("api.urls")),
     path("accounts/", include("accounts.urls")),
     
     path('login/',
@@ -34,5 +34,9 @@ urlpatterns = [
          LogoutView.as_view(),
          name='logout_urlpattern'),
     
+    path('signup/',
+        SignupView.as_view(template_name='accounts/signup.html'),
+        name='signup_urlpattern'),
+
     path('accounts/', include('allauth.urls')),
 ]
