d9e1f7ad77770bca27ccf6a2ee69c020a37ea8dc
Byu33 <byu33@illinois.edu>
2026-02-27 00:12:44 -0600

    added api.urls

--- SubFlo/urls.py
+++ SubFlo/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("dashboard/", include("dashboard.urls")),
     path("accounts/", include("accounts.urls")),
+    path("api/", include("api.urls")),
     
     path('login/',
          LoginView.as_view(template_name='accounts/login.html'),
